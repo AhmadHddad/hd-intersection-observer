f08da2312cfbf383dccc0666a29ed501c2afe956
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-12-25 17:44:05 +0200

    adding support for multible elements and setting default options

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@
 
 export type ObserverInstanceCallback = (
   inView: boolean,
-  entry: IntersectionObserverEntry
+  entry: IntersectionObserverEntry,
+  entries?: IntersectionObserverEntry[]
 ) => void;
 
 const observerMap = new Map<
@@ -36,8 +37,8 @@ export function defaultFallbackInView(inView: boolean | undefined) {
  * Generate a unique ID for the root element
  * @param root
  */
-function getRootId(root: IntersectionObserverInit['root']) {
-  if (!root) return '0';
+function getRootId(root: IntersectionObserverInit["root"]) {
+  if (!root) return "0";
   if (RootIds.has(root)) return RootIds.get(root);
   rootId += 1;
   RootIds.set(root, rootId.toString());
@@ -52,10 +53,10 @@ function getRootId(root: IntersectionObserverInit['root']) {
 export function optionsToId(options: IntersectionObserverInit): string {
   return Object.keys(options)
     .sort()
-    .filter(key => options[key as keyof IntersectionObserverInit] !== undefined)
-    .map(key => {
+    .filter((key) => options[key as keyof IntersectionObserverInit] !== undefined)
+    .map((key) => {
       return `${key}_${
-        key === 'root'
+        key === "root"
           ? getRootId(options.root)
           : options[key as keyof IntersectionObserverInit]
       }`;
@@ -65,7 +66,7 @@ export function optionsToId(options: IntersectionObserverInit): string {
 
 function createObserver(options: IntersectionObserverInit) {
   // Create a unique ID for this observer instance, based on the root, root margin and threshold.
-  let id = optionsToId(options);
+  const id = optionsToId(options);
   let instance = observerMap.get(id);
 
   if (!instance) {
@@ -73,23 +74,23 @@ function createObserver(options: IntersectionObserverInit) {
     const elements = new Map<Element, Array<ObserverInstanceCallback>>();
     let thresholds: number[] | readonly number[];
 
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
         // While it would be nice if you could just look at isIntersecting to determine if the component is inside the viewport, browsers can't agree on how to use it.
         // -Firefox ignores `threshold` when considering `isIntersecting`, so it will never be false again if `threshold` is > 0
         const inView =
           entry.isIntersecting &&
-          thresholds.some(threshold => entry.intersectionRatio >= threshold);
+          thresholds.some((threshold) => entry.intersectionRatio >= threshold);
 
         // @ts-ignore support IntersectionObserver v2
-        if (options.trackVisibility && typeof entry.isVisible === 'undefined') {
+        if (options.trackVisibility && typeof entry.isVisible === "undefined") {
           // The browser doesn't support Intersection Observer v2, falling back to v1 behavior.
           // @ts-ignore
           entry.isVisible = inView;
         }
 
-        elements.get(entry.target)?.forEach(callback => {
-          callback(inView, entry);
+        elements.get(entry.target)?.forEach((callback) => {
+          callback(inView, entry, entries);
         });
       });
     }, options);
@@ -97,9 +98,7 @@ function createObserver(options: IntersectionObserverInit) {
     // Ensure we have a valid thresholds array. If not, use the threshold from the options
     thresholds =
       observer.thresholds ||
-      (Array.isArray(options.threshold)
-        ? options.threshold
-        : [options.threshold || 0]);
+      (Array.isArray(options.threshold) ? options.threshold : [options.threshold || 0]);
 
     instance = {
       id,
@@ -114,7 +113,7 @@ function createObserver(options: IntersectionObserverInit) {
 }
 
 /**
- * @param element - DOM Element to observe
+ * @param elementOrElements - DOM Element or Elements to observe
  * @param callback - Callback function to trigger when intersection status changes
  * @param options - Intersection Observer options
  * @param fallbackInView - Fallback inView value.
@@ -138,25 +137,33 @@ observer();
  * @return Function - Cleanup function that should be triggered to unregister the observer
  */
 export default function observe(
-  element: Element,
+  elementOrElements: Element | Element[],
   callback: ObserverInstanceCallback,
-  options: IntersectionObserverInit = {},
+  options: IntersectionObserverInit = {
+    rootMargin: "0px",
+    threshold: 0.5,
+  },
   fallbackInView = unsupportedValue
 ) {
+  const allElements = Array.isArray(elementOrElements)
+    ? elementOrElements
+    : [elementOrElements];
+
   if (
-    typeof window.IntersectionObserver === 'undefined' &&
+    typeof window.IntersectionObserver === "undefined" &&
     fallbackInView !== undefined
   ) {
-    const bounds = element.getBoundingClientRect();
-    callback(fallbackInView, {
-      isIntersecting: fallbackInView,
-      target: element,
-      intersectionRatio:
-        typeof options.threshold === 'number' ? options.threshold : 0,
-      time: 0,
-      boundingClientRect: bounds,
-      intersectionRect: bounds,
-      rootBounds: bounds,
+    allElements.forEach((element) => {
+      const bounds = element.getBoundingClientRect();
+      callback(fallbackInView, {
+        isIntersecting: fallbackInView,
+        target: element,
+        intersectionRatio: typeof options.threshold === "number" ? options.threshold : 0,
+        time: 0,
+        boundingClientRect: bounds,
+        intersectionRect: bounds,
+        rootBounds: bounds,
+      });
     });
     return () => {
       // Nothing to cleanup
@@ -165,29 +172,34 @@ export default function observe(
   // An observer with the same options can be reused, so lets use this fact
   const { id, observer, elements } = createObserver(options);
 
-  // Register the callback listener for this element
-  let callbacks = elements.get(element) || [];
-  if (!elements.has(element)) {
-    elements.set(element, callbacks);
-  }
+  allElements.forEach((element) => {
+    // Register the callback listener for this element
+    let callbacks = elements.get(element) || [];
+    if (!elements.has(element)) {
+      elements.set(element, callbacks);
+    }
 
-  callbacks.push(callback);
-  observer.observe(element);
+    callbacks.push(callback);
+    observer.observe(element);
+  });
 
   return function unobserve() {
-    // Remove the callback from the callback list
-    callbacks.splice(callbacks.indexOf(callback), 1);
+    allElements.forEach((element) => {
+      let callbacks = elements.get(element) || [];
+      // Remove the callback from the callback list
+      callbacks.splice(callbacks.indexOf(callback), 1);
 
-    if (callbacks.length === 0) {
-      // No more callback exists for element, so destroy it
-      elements.delete(element);
-      observer.unobserve(element);
-    }
+      if (callbacks.length === 0) {
+        // No more callback exists for element, so destroy it
+        elements.delete(element);
+        observer.unobserve(element);
+      }
 
-    if (elements.size === 0) {
-      // No more elements are being observer by this instance, so destroy it
-      observer.disconnect();
-      observerMap.delete(id);
-    }
+      if (elements.size === 0) {
+        // No more elements are being observer by this instance, so destroy it
+        observer.disconnect();
+        observerMap.delete(id);
+      }
+    });
   };
 }
